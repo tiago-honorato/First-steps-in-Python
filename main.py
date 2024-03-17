0f1bd3154934566aeed3ecdb69ab2112f90cae50
Tiago Honorato <85461392+tiago-honorato@users.noreply.github.com>
2024-03-17 01:37:14 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+class HelloWorld:
+    def say_hello(self):
+        print("Hello, world!")
+
+# Criando uma instância da classe HelloWorld
+objeto_hello = HelloWorld()
+
+# Chamando o método say_hello para imprimir "Hello, world!"
+objeto_hello.say_hello()
