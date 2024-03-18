70ee73b3c980da2e7272619a9ac40353f2f8fb98
Tiago Honorato <85461392+tiago-honorato@users.noreply.github.com>
2024-03-18 14:04:13 -0300

    Update POO.py

--- POO.py
+++ POO.py
@@ -1,9 +1,12 @@
+# Declaração da classe
 class HelloWorld:
+    # Define um método(def = define)
     def say_hello(self):
+        
         print("Hello, world!")
-
-# Criando uma instância da classe HelloWorld
+        
+# Cria uma instância da classe HelloWorld
 objeto_hello = HelloWorld()
 
-# Chamando o método say_hello para imprimir "Hello, world!"
+# Chama o método say_hello para imprimir "Hello, world!"
 objeto_hello.say_hello()
