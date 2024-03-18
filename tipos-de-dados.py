97a80a5ce2eb4ee9d1c4256dea7b2806fefe7978
Tiago Honorato <85461392+tiago-honorato@users.noreply.github.com>
2024-03-18 03:59:34 -0300

    Add files via upload

---
+++ tipos-de-dados.py
@@ -0,0 +1,18 @@
+# Não há necessidade de uma função main em python.
+# A função principal em Python não precisa ser explicitamente definida.
+# Se o script Python for executado, o código será executado diretamente, sem precisar compilar explicitamente.
+
+'''
+Não há necessidade de declarar explicitamente o tipo de dados de uma variável ao atribuir um valor a ela.
+O tipo de dados é inferido automaticamente pelo interpretador Python com base no valor atribuído.
+'''
+inteiro = 10
+numeroComVirgula = 2.10
+dupla_precisao = 3.141592653589
+caractere = 'A'
+texto = "Olá, mundo!"
+verdadeiro = True
+# Não há constante em python, mas por convenção são declaradas em UPPERCASE
+VALOR_PI = 3.14159
+
+
