e26e3fbde467bda5ad3fccd52f58839d5e891626
Tiago Honorato <85461392+tiago-honorato@users.noreply.github.com>
2024-03-18 14:09:46 -0300

    Create estrutura-de-repeticao.py

---
+++ estrutura-de-repeticao.py
@@ -0,0 +1,17 @@
+# Estrutura FOR
+for i in range(1, 6):
+    print("estrutura FOR:", i)
+
+# Estrutura WHILE
+contadorWhile = 1
+while contadorWhile <= 5:
+    print("estrutura WHILE:", contadorWhile)
+    contadorWhile += 1
+
+# Estrutura DO-WHILE
+contadorDoWhile = 1
+while True:
+    print("estrutura DO-WHILE:", contadorDoWhile)
+    contadorDoWhile += 1
+    if contadorDoWhile > 5:
+        break
